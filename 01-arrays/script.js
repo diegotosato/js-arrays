5d493c467228e32b2f7dd8d5f776e11850413b37
diegotosato <tosato1998@gmail.com>
2025-10-08 15:09:24 +0200

    complete task 5
    add new array element at the end by using array method .push()

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -8,15 +8,26 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
+
+
+
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
 
 
+
+
+
+
+
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers[4] = 'Patrick'
 console.log(teachers);
+
+
+
 
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
@@ -34,7 +45,20 @@ console.log(firstTeacher);
 
 console.log(teachers);
 
+
+
+
+
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push('Vanessa');
+
+console.log(teachers);
+
+
+
+
+
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
